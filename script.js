dec1042076d4b8170ce08b435274dfcded3c5408
EugenCoda <coda.eugen@gmail.com>
2021-05-06 12:51:25 +0200

    finished challenge

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let menuBtn = document.querySelector(".menu-btn");
+let closeBtn = document.querySelector(".close-btn");
 let navMenu = document.querySelector(".nav-menu");
 
 menuBtn.addEventListener("click", function () {
   navMenu.classList.toggle("active");
+  menuBtn.style.display = "none";
+  closeBtn.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+  navMenu.classList.toggle("active");
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "block";
 });
